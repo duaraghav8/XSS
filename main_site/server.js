3f999503a99cfe3df988156129dfa9d19926d149
duaraghav8@gmail <duaraghav8@gmail.com>
2016-04-15 14:05:12 +0530

    addedd cookie functionality

--- main_site/server.js
+++ main_site/server.js
@@ -8,14 +8,8 @@ var express = require ('express'),
   serveStatic = require ('serve-static'),
   session = require ('express-session'),
   commentModel = require ('mongoose').model ('comments'),
-  app = express ();
-/*  comments = [
-    'How can I become even 1/10th as awesome as Raghav? Is it even humanely possible?',
-    'Vatsal is finishing all of this world\'s food',
-    'Shubrika is a potato.',
-    'Mridul stop tickling people they\'ll kill you one day',
-    'Where can I hide a dead body? I need answer urgently (just out of curiosity, nothing else)'
-  ];*/
+  app = express (),
+  counter = 1;
 
   //<script>$.ajax({type: 'POST', data: JSON.stringify({userInfo: document.cookie}), contentType: 'application/json', url: 'http://localhost:8085/'});</script>
 
@@ -29,12 +23,18 @@ var express = require ('express'),
 
 app
   .use (bodyParser.json ())
+  .use (bodyParser.urlencoded ({
+    extended: true
+  }))
+  .use (cookieParser ())
   .use (serveStatic (__dirname))
   .set ('views', __dirname)
   .set ('view engine', 'ejs');
 
 app
   .get ('/', function (req, res) {
+    if (!req.cookies.counter) { res.cookie ('counter', counter++); }
+
     commentModel.find ({}, {comment: 1}, function (err, comments) {
       if (err) {return (res.sendStatus (500));}
 
